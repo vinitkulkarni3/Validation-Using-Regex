ab1291a4193b1e0e89655e7fa45a0a605956412c
vinitkulkarni3 <vinitkulkarni4321@gmail.com>
2020-12-18 09:47:17 +0530

    Added: Welcome message

---
+++ src/com/regexvalidation/RegexValidation.java
@@ -0,0 +1,10 @@
+
+package com.regexvalidation;
+
+public class RegexValidation 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("* * * WELCOME TO USER REGISTRATION USING REGEX * * *");
+    }   
+}
