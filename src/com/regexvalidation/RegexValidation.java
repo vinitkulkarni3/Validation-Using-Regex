d4acdca72e153fe6f41753e572da8780c6554432
vinitkulkarni3 <vinitkulkarni4321@gmail.com>
2020-12-18 11:16:34 +0530

    Refactor: Used Function for Validation

--- src/com/regexvalidation/RegexValidation.java
+++ src/com/regexvalidation/RegexValidation.java
@@ -6,81 +6,110 @@ import java.util.regex.Pattern;
 
 public class RegexValidation 
 {
-    public static void main(String[] args) 
+    String firstName;
+    String lastName;
+    String email;
+    String mobileNumber;
+    String password;
+    
+    Scanner readInput = new Scanner(System.in);
+    
+    public void firstNameValidation()
     {
-        System.out.println("* * * WELCOME TO USER REGISTRATION USING REGEX * * *\n");
-        
-        String firstName;
-        String lastName;
-        String email;
-        String mobileNumber;
-        String password;
-        Scanner readInput = new Scanner(System.in);
-        
-        //FirstName Validation
-        String firstNamePattern = "^[A-Z]{1}[a-z]{2,}$";
+        String firstNamePattern ="^[A-Z]{1}[a-z]{2,}$";
         System.out.print("Enter the First Name : ");
         firstName  = readInput.next();
         if(Pattern.matches(firstNamePattern, firstName))
         {
-            System.out.println("Valid FirstName");
+            System.out.println("✔");
+            lastNameValidation();
         }
         else
         {
             System.out.println("Invalid FirstName");
+            firstNameValidation();
         }
-        
-        //LastName Validation
+    }
+    
+    public void lastNameValidation()
+    {
         String lastNamePattern = "^[A-Z]{1}[a-z]{2,}$";
         System.out.print("Enter the Last Name : ");
         lastName  = readInput.next();
         if(Pattern.matches(lastNamePattern, lastName))
         {
-            System.out.println("Valid LastName");
+            System.out.println("✔");
+            emailValidation();
         }
         else
         {
             System.out.println("Invalid LastName");
+            lastNameValidation();
         }
-        
-        //Email Validation
+    }
+    
+    public void emailValidation()
+    {
         String emailPattern = "^[a-zA-Z0-9\\\\-\\\\+\\\\.]+.([a-zA-Z0-9])*@([a-z0-9]+.[a-z]{2,}.([a-z]{2,})?)$";
         System.out.print("Enter the Email ID : ");
         email  = readInput.next();
         if(Pattern.matches(emailPattern, email))
         {
-            System.out.println("Valid Email");
+            System.out.println("✔");
+            mobileNumberValidation();
         }
         else
         {
             System.out.println("Invalid Email");
+            emailValidation();
         }
-        
-        //MobileNumber Validation
+    }
+    
+    public void mobileNumberValidation()
+    {
         String mobilePattern = "^[0-9]{2}[-]?[0-9]{10}$";
         System.out.print("Enter the Mobile Number :");
         mobileNumber  = readInput.next();
         if(Pattern.matches(mobilePattern, mobileNumber))
         {
-            System.out.println("Valid Number");
+            System.out.println("✔");
+            passwordValidation();
         }
         else
         {
             System.out.println("Invalid Number");
+            mobileNumberValidation();
         }
-        
-        //Password Validation
+    }
+    
+    public void passwordValidation()
+    {
         String passwordPattern = "^(?=[A-Za-z0-9!@#]{8,}$)(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).*";
         System.out.println("RULES: [Minimum length is 8] [1 specialChar] [Atleast 1 Cap , Lower, digit]");
         System.out.print("Enter the Password :");
         password  = readInput.next();
         if(Pattern.matches(passwordPattern, password))
         {
-            System.out.println("Valid Password");
+            System.out.println("✔");
+            display();
         }
         else
         {
             System.out.println("Invalid Password");
+            passwordValidation();
         }
+    }
+    
+    public void display()
+    {
+        System.out.println("\nUSER REGISTRATION SUCCSSFULL WITH NAME -> "+firstName);
+    }
+    
+    public static void main(String[] args) 
+    {
+        System.out.println("* * * WELCOME TO USER REGISTRATION USING REGEX * * *\n");
+            
+        RegexValidation regexObject = new RegexValidation();
+        regexObject.firstNameValidation();
     }   
 }
