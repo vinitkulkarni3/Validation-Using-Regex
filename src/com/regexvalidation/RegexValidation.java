36d9564f89d92dc1805c4c00fba9de625d6f5c9b
vinitkulkarni3 <vinitkulkarni4321@gmail.com>
2020-12-18 09:55:22 +0530

    Added: FirstName Validation

--- src/com/regexvalidation/RegexValidation.java
+++ src/com/regexvalidation/RegexValidation.java
@@ -1,10 +1,29 @@
 
 package com.regexvalidation;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class RegexValidation 
 {
     public static void main(String[] args) 
     {
-        System.out.println("* * * WELCOME TO USER REGISTRATION USING REGEX * * *");
+        System.out.println("* * * WELCOME TO USER REGISTRATION USING REGEX * * *\n");
+        
+        String firstName;
+        Scanner readInput = new Scanner(System.in);
+        
+        //FirstName Validation
+        String firstNamePattern = "^[A-Z]{1}[a-z]{2,}$";
+        System.out.print("Enter the First Name : ");
+        firstName  = readInput.next();
+        if(Pattern.matches(firstNamePattern, firstName))
+        {
+            System.out.println("Valid FirstName");
+        }
+        else
+        {
+            System.out.println("Invalid FirstName");
+        }
     }   
 }
